6461aafca562eeb94b6f2336ff954775336f867b
Vikrant Pise <vikrantpise98@gmail.com>
2024-09-04 03:11:58 +0530

    add mute unmute button

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
-    setTimeout(function () {
-      
-      document.getElementById("preloader").style.display = "none";
-     
-      document.getElementById("main").style.display = "block";
-    }, 1500); 
+  setTimeout(function () {
+    document.getElementById("preloader").style.display = "none";
+    document.getElementById("main").style.display = "block";
+  }, 1500);
+
+  // Unmute button
+  const muteButton = document.getElementById("mute-button");
+  const video = document.getElementById("background-video");
+
+  muteButton.addEventListener("click", function () {
+    if (video.muted) {
+      video.muted = false;
+      muteButton.textContent = "🔊"; 
+    } else {
+      video.muted = true;
+      muteButton.textContent = "🔈"; 
+    }
   });
- 
+});
